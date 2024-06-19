d1e3a2a62324ae0d82994dafa7614bc748ae1255
Riyaa26 <122543538+Riyaa26@users.noreply.github.com>
2024-06-19 19:49:18 +0530

    secondquestion.py

---
+++ secondquestion.py
@@ -0,0 +1,6 @@
+def q2():
+    num = int(input("enter number to check it is even or odd : "))
+    if num % 2 == 0 : 
+        print("{} is even".format(num))
+    else :
+        print("{} is odd".format(num))
